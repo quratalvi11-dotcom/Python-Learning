2a5e5bbffb486be4cfa5d0b7275a730562ab8258
Qurat Ul Ain <qurat.alvi11@gmail.com>
2026-08-24 10:38:08 +0000

    Complete Day 1: Introduction and Data Types

---
+++ day_01_introduction.py
@@ -0,0 +1,17 @@
+#  1. Printing Output 
+print("Hello, Machine Learning World")
+
+#  2. Basic Data Types 
+print(type(12))        #integer(int): whole number 
+print(type(3.14))      #Float: decimal numbers
+print(type("python"))  #string(str): Text Data
+print(type(True))      #boolean: True or False value
+print(type([1, 2, 3])) #List: ordered sequence of an element
+
+#  3. Simple Mathematical Operations 
+print(5 + 3)   #addition -> 8
+print(10 - 3)  #subtraction -> 7
+print(6 * 8)   #multiplication -> 48
+print(15 / 3)  #division -> 5.0(always in float)
+print(10 % 4)  #modulus (remainder) -> 2
+print(2 ** 3)  #exponentiation -> (2^3)= 8
